71e06b6b85a61322070eebc63f26c784d7b2553d
xsfen <77191712+xsfen@users.noreply.github.com>
2025-09-04 18:12:35 +0800

    Add files via upload

---
+++ APConv.py
@@ -0,0 +1,114 @@
+######################################## APConv AAAI2025   by AI Little monster start  ########################################
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ultralytics.nn.modules.conv import Conv, autopad
+from ultralytics.nn.modules.block import Bottleneck, C2f, C3k, C3k2
+
+class PConv(nn.Module):  
+    ''' Pinwheel-shaped Convolution using the Asymmetric Padding method. '''
+    
+    def __init__(self, c1, c2, k, s):
+        super().__init__()
+        p = [(k, 0, 1, 0), (0, k, 0, 1), (0, 1, k, 0), (1, 0, 0, k)]
+        self.pad = [nn.ZeroPad2d(padding=(p[g])) for g in range(4)]
+        self.cw = Conv(c1, c2 // 4, (1, k), s=s, p=0)
+        self.ch = Conv(c1, c2 // 4, (k, 1), s=s, p=0)
+        self.cat = Conv(c2, c2, 2, s=1, p=0)
+
+    def forward(self, x):
+        yw0 = self.cw(self.pad[0](x))
+        yw1 = self.cw(self.pad[1](x))
+        yh0 = self.ch(self.pad[2](x))
+        yh1 = self.ch(self.pad[3](x))
+        return self.cat(torch.cat([yw0, yw1, yh0, yh1], dim=1))
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+
+class Bottleneck(nn.Module):
+    """Standard bottleneck."""
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a standard bottleneck module with optional shortcut connection and configurable parameters."""
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        """Applies the YOLO FPN to input data."""
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+class C3k_PConv(nn.Module):
+    """C3k is a CSP bottleneck module with customizable kernel sizes for feature extraction in neural networks."""
+    
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5, k=3):
+        """Initializes the C3k module with specified channels, number of layers, and configurations."""
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = PConv(c1, c_, 3, 1)
+        self.cv2 = PConv(c1, c_, 3, 1)
+        self.cv3 = PConv(2 * c_, c2, 3, 1)
+        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, k=(k, k), e=1.0) for _ in range(n)))
+
+    def forward(self, x):
+        """Forward pass through the CSP bottleneck."""
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
+
+class C3k2_PConv(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+    
+    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
+        """Initializes the C3k2 module, a faster CSP Bottleneck with 2 convolutions and optional C3k blocks."""
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)
+        self.m = nn.ModuleList(
+            C3k_PConv(self.c, self.c, 2, shortcut, g) for _ in range(n)
+        )
+    
+    def forward(self, x):
+        """Forward pass through C3k2_PConv layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+    
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+if __name__ == '__main__':
+    x = torch.randn(1, 64, 224, 224)
+    conv = PConv(64, 128, k=3)
+    print(conv(x).shape)  # 应输出 torch.Size([1, 128, 224, 224])
---
+++ MSCA.py
@@ -0,0 +1,90 @@
+
+ 
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from ultralytics.nn.modules.conv import Conv
+from ultralytics.nn.modules.block import Bottleneck, C2f,C3k
+
+ 
+class MSCAAttention(nn.Module):
+    # SegNext NeurIPS 2022
+    # https://github.com/Visual-Attention-Network/SegNeXt/tree/main
+    def __init__(self, dim):
+        super().__init__()
+        self.conv0 = nn.Conv2d(dim, dim, 5, padding=2, groups=dim)
+        self.conv0_1 = nn.Conv2d(dim, dim, (1, 7), padding=(0, 3), groups=dim)
+        self.conv0_2 = nn.Conv2d(dim, dim, (7, 1), padding=(3, 0), groups=dim)
+ 
+        self.conv1_1 = nn.Conv2d(dim, dim, (1, 11), padding=(0, 5), groups=dim)
+        self.conv1_2 = nn.Conv2d(dim, dim, (11, 1), padding=(5, 0), groups=dim)
+ 
+        self.conv2_1 = nn.Conv2d(dim, dim, (1, 21), padding=(0, 10), groups=dim)
+        self.conv2_2 = nn.Conv2d(dim, dim, (21, 1), padding=(10, 0), groups=dim)
+        self.conv3 = nn.Conv2d(dim, dim, 1)
+ 
+    def forward(self, x):
+        u = x.clone()
+        attn = self.conv0(x)
+ 
+        attn_0 = self.conv0_1(attn)
+        attn_0 = self.conv0_2(attn_0)
+ 
+        attn_1 = self.conv1_1(attn)
+        attn_1 = self.conv1_2(attn_1)
+ 
+        attn_2 = self.conv2_1(attn)
+        attn_2 = self.conv2_2(attn_2)
+        attn = attn + attn_0 + attn_1 + attn_2
+ 
+        attn = self.conv3(attn)
+ 
+        return attn * u
+ 
+class C2f_MSCA(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+        expansion.
+        """
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+        self.att = MSCAAttention(c2)
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.att(self.cv2(torch.cat(y, 1)))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.att(self.cv2(torch.cat(y, 1)))
+
+class C3k2_MSCA(C2f_MSCA):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
+        """Initializes the C3k2 module, a faster CSP Bottleneck with 2 convolutions and optional C3k blocks."""
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(
+            C3k(self.c, self.c, 2, shortcut, g) if c3k else Bottleneck(self.c, self.c, shortcut, g) for _ in range(n)
+        )
+ 
+
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
